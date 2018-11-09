4d50241eecc6479f2a02227c94e9e6d554f80567
Washington <221032910@logon.ds.ge.com>
2018-11-09 11:07:14 -0500

    update

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,9 +1,59 @@
-// heart animate
-$('.like-btn').on('click', function() {
-    $(this).toggleClass('is-active');
- });
+$(document).ready(function () {
+    let num1;
+    let num2;
+    let result;
+    let operator;
+    let isFirstComplete = false;
+  
 
-//  quantity buttons work
+const setNumber = function (e) {
+    e.preventDefault();
+    const digit = parseInt($(this).val());
+    if (!isFirstComplete) {
+      num1 = num1 || 0;
+      num1 = (num1 * 10) + digit; 
+    } else {
+      num2 = num2 || 0;
+      num2 = (num2 * 10) + digit;
+    }
+
+    render();
+  }
+
+
+/* 
+  operator and input based on plus or minus.
+  */
+ const setResult = function (e) {
+    e.preventDefault();
+    switch (operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+    }
+
+    render();
+  }
+
+   /* 
+  Reset to its initial state.
+  */
+ const clear = function(e){
+    e.preventDefault();
+    num1 = '';
+    num2 = '';
+    operator = '';
+    result = '';
+    isFirstComplete = false;
+
+    render();
+  }
+
+}
+
 $('.minus-btn').on('click', function(e) {
     e.preventDefault();
     var $this = $(this);
